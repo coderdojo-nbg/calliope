f8f6212c10ace64a7c926f04c032f46d0d773b80
Frank Schmittlein <mail@frank-schmittlein.de>
2017-06-18 22:38:58 +0200

    Beispiel aus dem Cornelsen Verlag

---
+++ TaktgeberErweiterung.cpp
@@ -0,0 +1,37 @@
+#define _GNU_SOURCE
+
+#include "MicroBit.h" 
+#include <array>
+#include <stdlib.h>
+MicroBit uBit;
+
+
+int initTime = uBit.systemTime(); 
+
+double wartezeit = 500;
+
+int main() 
+{
+    uBit.init();
+    
+    while ( true ) {
+        uBit.rgb.setColour(MicroBitColor(255, 0, 0, 255));
+        uBit.sleep(50);
+        uBit.rgb.off();
+        uBit.sleep(wartezeit);
+        if ( uBit.buttonA.isPressed() ) {
+            wartezeit = wartezeit - 5;
+            if ( wartezeit < 300 ) {
+                wartezeit = 300;
+            }
+        }
+        if ( uBit.buttonB.isPressed() ) {
+            wartezeit = wartezeit + 5;
+            if ( wartezeit > 1500 ) {
+                wartezeit = 1500;
+            }
+        }
+        uBit.sleep(1);
+    }
+    release_fiber();
+}
